3713a5c93bdc441f0a6aa06065963650edba6192
ejkim9090 <ej.kh.kim@gmail.com>
2026-08-19 23:03:08 +0900

    streamlit 사용해 날씨 정보 웹으로 확인

--- app.py
+++ app.py
@@ -1,5 +1,45 @@
+import pandas as pd
 import streamlit as st
 
-st.title("Climate AI Newsroom")
+st.set_page_config(
+    page_title="Climate AI Newsroom",
+    layout="wide"
+)
 
-st.write("환경 설정 성공")
+st.title("🌤 Climate AI Newsroom")
+
+df = pd.read_csv("data/weather.csv")
+
+row = df.iloc[0]
+
+st.subheader("현재 기상 정보")
+
+col1, col2, col3, col4 = st.columns(4)
+
+col1.metric(
+    "현재 기온",
+    f"{row['temperature']} ℃"
+)
+
+col2.metric(
+    "체감 온도",
+    f"{row['feels_like']} ℃"
+)
+
+col3.metric(
+    "습도",
+    f"{row['humidity']} %"
+)
+
+col4.metric(
+    "풍속",
+    f"{row['wind_speed']} m/s"
+)
+
+st.divider()
+
+st.subheader("기상 상태")
+
+st.write(
+    f"현재 {row['city']}의 날씨는 **{row['weather']}** 입니다."
+)
